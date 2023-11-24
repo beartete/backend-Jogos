d3b8c7474dc0325cc81398661fd6179fac8de66e
beartete <isabellacristinavieira2003@gmail.com>
2023-11-24 08:50:55 -0300

    24-10

--- api/index.js
+++ api/index.js
@@ -1,16 +1,23 @@
 import express from 'express'
 
+import cors from 'cors'
 const app = express()
-const port = 4000
-//import das rotas da aplicação
+const port = 4000 
+//import das rotas da app
 import rotasFilmes from './routes/filme.js'
 
+//Habilita o CORS Cross-Origin resource sharing
+app.use(cors({
+    origin: ['http://127.0.0.1:5500','http://localhost:4000'] //informe outras URL´s se necessário
+  }));
 app.use(express.json()) // irá fazer o parse de arquivos JSON
+
 //Rotas de conteúdo público
 app.use('/', express.static('public'))
 
+
 //Configura o favicon
-app.use('/favicom.ico', express.static('public/images/icon.videogame.png'))
+app.use('/favicon.ico', express.static('public/images/filme.png'))
 
 //Rotas da API
 app.use('/api/filmes', rotasFilmes)
@@ -21,16 +28,16 @@ app.get('/api', (req, res) => {
         version: '1.0.1'
     })
 })
-//Rotas de exceção - deve ser a última
+//Rotas de Exceção - deve ser a última!
 app.use(function (req, res) {
     res.status(404).json({
         errors: [{
             value: `${req.originalUrl}`,
             msg: `A rota ${req.originalUrl} não existe nesta API!`,
             param: 'invalid route'
-}]
+        }]
     })
 })
 app.listen(port, function(){
-    console.log(` Servidor rodando na porta ${port}`)
+    console.log(`🖥 Servidor rodando na porta ${port}`)
 })
--- api/routes/filme.js
+++ api/routes/filme.js
@@ -3,22 +3,26 @@
 import express, { Router } from 'express'
 import { connectToDatabase } from '../utils/mongodb.js'
 import { check, validationResult } from 'express-validator'
+//JWT
+import auth from '../middleware/auth.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 const {db, ObjectId} = await connectToDatabase()
 const nomeCollection = 'filmes'
 
 const validaFilme = [
-    check('#TITLE')
+    check('title')
     .not().isEmpty().trim().withMessage('É obrigatório informar o nome do filme')
     .isAlphanumeric('pt-BR', {ignore: '/. '})
     .isLength({min:1}).withMessage('O nome do filme deve ter pelo menos um caractere')
     .isLength({max:200}).withMessage('O nome do filme é muito longo. Máximo 200'),
-    check('YEAR')
+    check('year')
     .not().isEmpty().trim().withMessage('É obrigatório informar o ano')
     .isNumeric().withMessage('O ano deve conter números')
     .isLength({ min: 4, max: 4 }).withMessage('O ano deve conter 4 números'),
-    check('#IMDB_ID')
+    check('imdb_id')
     .not().isEmpty().trim().withMessage('É obrigatório informar o IMDB id do filme')
     .isAlphanumeric('pt-BR', {ignore: '/. '})
     .custom(value => {
@@ -28,16 +32,21 @@ const validaFilme = [
         return true;
     })
     .isLength({min:7, max:7}).withMessage('O imdb deve conter 7 caracteres'),
-    check('RANK')
+    check('rank')
     .not().isEmpty().trim().withMessage('É obrigatório informar o rank do filme')
     .isNumeric().withMessage('O rank deve conter apenas números')
     .isLength({ min: 1, max: 6 }).withMessage('O rank pode conter até 6 caracteres'),
-    check('#ACTORS')
+    check('actors')
     .not().isEmpty().trim().withMessage('É obrigatório informar o nome dos atores')
     .isAlphanumeric('pt-BR', {ignore: '/. '})
     .isLength({min:1}).withMessage('O filme deve conter pelo menos um nome')
     .isLength({max:10000}).withMessage('Já há muitos nomes. máximo 10000 caracteres'),
-    check('#AKA').optional({nullable: true})
+    check('aka').optional({nullable: true}),
+    check('director')
+    .not().isEmpty().trim().withMessage('É obrigatório informar o nome do diretor do filme')
+    .isAlphanumeric('pt-BR', {ignore: '/. '})
+    .isLength({min:1}).withMessage('O filme deve conter pelo menos um diretor')
+    .isLength({max:10000}).withMessage('No máximo 1000 caracteres')
 ] 
 
 /**
@@ -87,7 +96,7 @@ router.get('/id/:id', async(req, res) => {
 router.get('/title/:title', async(req, res) => {
     try{
         db.collection(nomeCollection)
-        .find({'#TITLE': {$regex: req.params.title, $options: "i"}})
+        .find({'title': {$regex: req.params.title, $options: "i"}})
         .ToArray((err, docs) => {
             if(err){
                 res.status(400).json(err)
@@ -147,6 +156,119 @@ router.put('/', validaFilme, async(req, res) => {
         await db.collection(nomeCollection)
         .updateOne({'_id': {$eq : ObjectId(idDocumento)}}, { $set: req.body})
         .then(result => res.status(200).send(result))
+        .catch(err => res.status(400).json(err))
+    }
+})
+
+/**************************
+ * POST filmes/cadastro
+ * Efetua o cadastro de um filme e retorna o token JWT
+ */
+
+const validaCadastro = [
+    check('title')
+    .not().isEmpty().trim().withMessage('É obrigatório informar o nome do filme')
+    .isAlphanumeric('pt-BR', {ignore: '/. '})
+    .isLength({min:1}).withMessage('O nome do filme deve ter pelo menos um caractere')
+    .isLength({max:200}).withMessage('O nome do filme é muito longo. Máximo 200'),
+    check('year')
+    .not().isEmpty().trim().withMessage('É obrigatório informar o ano')
+    .isNumeric().withMessage('O ano deve conter números')
+    .isLength({ min: 4, max: 4 }).withMessage('O ano deve conter 4 números')
+    ]
+
+    router.post('/cadastro', validaCadastro, async (req, res) => {
+        const schemaErrors = validationResult(req)
+        if (!schemaErrors.isEmpty()) {
+            return res.status(403).json(({ errors: schemaErrors.array() }))
+        }
+        //obtendo os valores para o cadastro
+        const { title, year } = req.body;
+        try {
+            //Verificando se o nome do filme informado existe no Mongodb
+            let filme = await db.collection(nomeCollection)
+                .find({ title }).limit(1).toArray()
+            //Se o array estiver vazio, é que o filme não existe
+            if (!filme.length)
+                return res.status(404).json({
+                    errors: [{
+                        value: `${title}`,
+                        msg: 'O filme informado não está cadastrado',
+                        param: 'title'
+                    }]
+                })
+            //Se o filme existir, comparamos se a ano está correta  
+            const isMatch = await bcrypt.compare(year, filme[0].year)
+            if (!isMatch)
+                return res.status(403).json({
+                    errors: [{
+                        value: `year`,
+                        msg: 'O ano informada está incorreta',
+                        param: 'year'
+                    }]
+                })
+            //Iremos gerar o token JWT
+            jwt.sign(
+                { filme: { id: filme[0]._id } },
+                process.env.SECRET_KEY,
+                { expiresIn: process.env.EXPIRES_IN },
+                (err, token) => {
+                    if (err) throw err
+                    res.status(200).json({
+                        access_token: token
+                    })
+                }
+            )
+        } catch (e) {
+            console.error(e)
+        }
+    })
+
+/************************************************************
+ * GET /filmes
+ * Lista todos os filmes. Necessita do token
+ ************************************************************/
+router.get('/', auth, async(req, res)=> {
+    try{
+        db.collection(nomeCollection)
+        .find({},{projection: { year: false}})
+        .sort({title:1})
+        .toArray((err, docs)=> {
+            if(!err){ res.status(200).json(docs)}
+        })
+    } catch (err){
+        res.status(500).json({errors: 
+            [{msg: 'Erro ao obter a listagem de filmes'}]})
+    }
+})
+
+/************************************************************
+ * DELETE /filmes/id
+ * Remove o usuário pelo id. Necessita do token
+ ************************************************************/
+router.delete('/:id', auth, async(req, res)=> {
+    await db.collection(nomeCollection)
+    .deleteOne({'_id': {$eq: ObjectId(req.params.id)}})
+    .then(result => res.status(202).send(result)) //accepted
+    .catch(err => res.status(400).json(err)) //bad request
+})
+
+/************************************************************
+ * PUT /filmes/id
+ * Altera os dados do filme pelo id. Necessita do token
+ ************************************************************/
+router.put('/:id', auth, validaFilme, async(req, res) => {
+    const schemaErrors = validationResult(req)
+    if(!schemaErrors.isEmpty()){
+        return res.status(403).json({
+            errors: schemaErrors.array()
+        })
+    } else {
+        await db.collection(nomeCollection)
+        .updateOne({'_id': {$eq: ObjectId(req.params.id)}},
+        { $set: req.body }
+        )
+        .then(result => res.status(202).send(result))
         .catch(err => res.status(400).json(err))
     }
 })
